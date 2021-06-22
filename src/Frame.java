db989e0f29cca52e7b1ba1e6f93f6f1fcf4cec30
Joey Smeets <joeysmeetsss@gmail.com>
2021-06-22 12:13:49 +0200

    Got as far as being able to create a single equilateral triangle without recursion.

--- src/Frame.java
+++ src/Frame.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import javax.swing.*;
 
 public class Frame extends JFrame {
@@ -7,6 +9,7 @@ public class Frame extends JFrame {
 	public Frame() {
 		this.setSize(1152, 1152);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setBackground(Color.WHITE);
 		this.add(graphics);
 		this.setVisible(true);
 	}
--- src/TriangleGraphics.java
+++ src/TriangleGraphics.java
@@ -2,7 +2,23 @@ import javax.swing.*;
 import java.awt.*;
 
 public class TriangleGraphics extends JPanel {
-
+	private Graphics2D g2D;
+	private int sideStart = 1152;
+	private int xStart = 0;
+	private int yStart  = 1152;
+	
+	private int x;
+	private int x2;
+	private int x3;
+			
+	private int y;
+	private int y2;
+	private int y3;
+	
+	private int xs[];
+	private int ys[];
+	
+	private int side = 1152;
 	
 	public TriangleGraphics() {
 		
@@ -10,11 +26,56 @@ public class TriangleGraphics extends JPanel {
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		this.setBackground(Color.BLACK);
 		
 		Graphics2D g2D = (Graphics2D) g;
 		
+		drawTriangle(xStart, yStart, sideStart);
+		
+		int[] xs = {x, x2, x3};
+		int[] ys = {y, y2, y3};
+		
 		g2D.setColor(Color.MAGENTA);
-		g2D.drawLine(0, 0, 400, 400);
+		g2D.drawPolygon(xs, ys, 3);
+		
+		
+//		g2D.setColor(Color.MAGENTA);
+//		g2D.drawPolygon(xs, ys, 3);
+		
+//		while (side > 10) {
+//			drawTriangle(x,          y,      side);
+//			drawTriangle(x+ side,    y,      side);
+//			drawTriangle(x+(side/2), y-side, side);
+//			
+//			side = side/2;
+//		}
+	}
+	
+	public void drawTriangle(int x, int y, int side) {
+//		xs.length = 0;
+//		ys.length = 0;
+		
+		this.x = x;
+		this.y = y;
+		
+		this.x2 = x + side/2;
+		this.y2 = y - side;
+		
+		this.x3 = x + side;
+		this.y3 = y;
+		
+//		xs[0] = this.x;
+//		xs[1] = x2;
+//		xs[2] = x3;
+//		ys[0] = this.y;
+//		ys[1] = y2;
+//		ys[2] = y3;
+		
+//		int[] xs = {x, x2, x3};
+//		int[] ys = {y, y2, y3};
+//		
+//		g2D.setColor(Color.MAGENTA);
+//		g2D.drawPolygon(xs, ys, 3);
 	}
 }
+	
+
